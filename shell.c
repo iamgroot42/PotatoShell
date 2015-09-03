b673f126d00a5378a1e00ec7e8c06418875dcc91
Anshuman Suri <anshuman14021@iiitd.ac.in>
2015-09-03 14:02:06 +0530

    Create shell.c

---
+++ shell.c
@@ -0,0 +1,70 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/wait.h>
+
+//@Author : Anshuman Suri , 2014021
+
+char LEL[100000]; //Maximum length of a word assumed to be 1e5
+char* temp[100]; //Maximum number of arguments 
+char* tok; //Tokenization 
+int i; //Temporary counter
+
+void ctrlC(int signum) //To handle Ctrl+C elegantly 
+{
+	kill(getpid(),0);
+}
+
+int main()
+{
+	printf("....Potato Shell.....\n"); //Welcome Screen
+	signal(SIGINT,ctrlC);
+	while(1)
+	{
+			printf("%s/$: ",getcwd(NULL,0));
+			fgets(LEL,100000,stdin); //Read input
+			i=0;
+			LEL[strlen(LEL)-1]='\0'; //Replace newline with \0
+			tok=strtok(LEL," "); //Break by space
+			if(tok==NULL) continue; //If blank,ignore (to avoid segmentation fault)
+			if(!strcmp("exit",strdup(tok))) break; //exit 
+			while(tok!=NULL)
+			{
+				temp[i++]=strdup(tok);
+				tok=strtok(NULL," ");
+			}
+			temp[i]=NULL;
+			int t=fork();
+			if(t<0)
+			{
+				printf("Fatal Error"); //Fork failed
+			}
+			else if(t>0)
+			{
+				wait(NULL); //Wait for child process
+			}
+			else
+			{
+				int tt=0;
+				tt=execvp(temp[0],temp); //Execute command
+				if(tt) //execvp() returns vale in case of failure
+				{
+					if(!strcmp(temp[0],"cd")) //If 'cd' command
+					{
+						int gg=chdir(temp[1]);
+						if(gg)
+						{
+							printf("Cannot access folder : %s\n",temp[1]); //Access right/Invalid folder problem
+						}
+					}
+					else
+					{
+						printf("Sorry, invalid command\n"); //If not in-built
+					}
+					break; //Read next command
+				}
+			}
+		}
+	return 0;
+}

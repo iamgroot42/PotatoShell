d36c3f92b1e41bf5798ecb54218637652ec4702f
Anshuman <anshuman14021@iiitd.ac.in>
2015-09-06 10:51:54 +0530

    Added file I/O redirection (not fully tested)

--- README.md
+++ README.md
@@ -2,7 +2,8 @@
 Custom Shell (made as part of OS assignment). 
 
 Supports:
--executables from /bin folder
+- file I/O redirection ('<' , '>')
+- executables from /bin folder
 - Ctrl+C interrupt
 - cd command
 - 'exit' to exit gracefully from the shell.
--- shell.c
+++ shell.c
@@ -3,6 +3,9 @@
 #include <unistd.h>
 #include <string.h>
 #include <sys/wait.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
 
 //@Author : Anshuman Suri , 2014021
 
@@ -10,7 +13,7 @@ char LEL[100000]; //Maximum length of a word assumed to be 1e5
 char* temp[100]; //Maximum number of arguments 
 char* tok; //Tokenization 
 int i; //Temporary counter
-static char* cirq[100]; //For 'history'
+static char* cirq[300]; //For 'history'
 static int front=0,rear=-1; //For circular queue
 
 void ctrlC(int signum) //To handle Ctrl+C elegantly 
@@ -18,13 +21,13 @@ void ctrlC(int signum) //To handle Ctrl+C elegantly
 	kill(getpid(),0);
 }
 
-void insert(char* x)
+void insert(char* x) //Insert() function for cirxular queue()
 {
-    if((front==0&&rear==99)||(front>0&&rear==front-1))
+    if((front==0&&rear==299)||(front>0&&rear==front-1))
         return;
     else
     {
-        if(rear==99&&front>0)
+        if(rear==299&&front>0)
         {
             rear=0;
             cirq[rear]=strdup(x);
@@ -37,7 +40,7 @@ void insert(char* x)
     }
 }
 
-void display()
+void display() //Display() function for circular queue
 {
     int I,J;
     if(front==0&&rear==-1)
@@ -48,7 +51,7 @@ void display()
     {
         for(I=0;I<=rear;I++)
             printf("%s\n",cirq[I]);
-        for(J=front;J<=99;J++)
+        for(J=front;J<=299;J++)
             printf("%s\n",cirq[J]);
     }
     else
@@ -63,18 +66,19 @@ void display()
 int main()
 {
 	printf("....Potato Shell.....\n"); //Welcome Screen
-	signal(SIGINT,ctrlC);
+	signal(SIGINT,ctrlC); //Readying to handle Ctrl+C interrupt
 	while(1)
 	{
-			printf("%s/$: ",getcwd(NULL,0));
+			printf("%s/$$: ",getcwd(NULL,0));
 			fgets(LEL,100000,stdin); //Read input
 			i=0;
+			int fraud=0; //Indicates presence/absence of I/O redirection
 			LEL[strlen(LEL)-1]='\0'; //Replace newline with \0
 			insert(LEL); //Insert to history
 			tok=strtok(LEL," "); //Break by space
 			if(tok==NULL) continue; //If blank,ignore (to avoid segmentation fault)
 			if(!strcmp("exit",strdup(tok))) break; //exit 
-			while(tok!=NULL)
+			while(tok!=NULL) 
 			{
 				temp[i++]=strdup(tok);
 				tok=strtok(NULL," ");
@@ -95,7 +99,7 @@ int main()
 					strcat(d,yolo);
 					char* cooking=temp[1]+1;
 					strcat(d,cooking);
-					temp[1]=strdup(d); //To handle the case ehwn only 'cd' or 'cd ~', which should take to user
+					temp[1]=strdup(d); //To handle the case ehwn only 'cd' or 'cd ~', which should take to /home/user
 				}
 				int gg=chdir(temp[1]);
 				if(gg)
@@ -116,13 +120,79 @@ int main()
 			else
 			{
 				int tt=0;
-				tt=execvp(temp[0],temp); //Execute command
+				int k,l;
+				char* redir[100]; //Extra array to store commands without redirection
+				l=0;
+				for(k=0;k<i;k++)
+				{
+					if(!strcmp(temp[k],"<"))
+					{
+						if(k<i-1)
+						{
+							int in;
+							fraud=1;
+							in=open(temp[k+1],O_RDONLY); //Set input file as I/O for process
+							if(in>0)
+							{
+								dup2(in, 0);
+								close(in);	
+								k++; //Exclude I/O file from execvp()
+							}
+							else
+							{
+								printf("Error opening file for input\n");
+								goto done; //Failed.Read next command
+							}
+						}
+						else
+						{
+							printf("Provide input file\n");
+							goto done; //Failed.Read next command
+						}
+					}
+					else if(!strcmp(temp[k],">"))
+					{
+						if(k<i-1)
+						{
+							int out;
+							fraud=1;
+							out=open(temp[k+1],O_WRONLY | O_TRUNC | O_CREAT, S_IRUSR | S_IRGRP | S_IWGRP | S_IWUSR); //Set output file as I/O for process
+							if(out>0)
+							{
+								dup2(out,1);
+								close(out);
+								k++; //Exclude I/O file from execvp()
+							}
+							else
+							{
+								printf("Error opening/creating file for output\n");
+								goto done; //Failed.Read next command
+							}
+						}
+						else
+						{
+							printf("Provide output file\n");
+							goto done; //Failed.Read next command
+						}
+					}
+					else
+					{
+						redir[l]=strdup(temp[k]); //Copy non I/O commands
+						l++;
+					}
+				}
+				redir[l]=NULL; //Set last as NULL
+				exec:
+				if(!fraud) tt=execvp(temp[0],temp); //Execute command
+				else tt=execvp(redir[0],redir); //Execute command with redirected I/O
 				if(tt) //execvp() returns vale in case of failure
 				{
 					printf("Sorry, invalid command\n"); //If not in-built
+					done:
 					break; //Read next command
 				}
 			}
+
 		}
 	return 0;
 }

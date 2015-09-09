b813d81d17d7a95b1a30910c881d80bb4bfdeba8
Anshuman <anshuman14021@iiitd.ac.in>
2015-09-09 22:38:15 +0530

    Single level pipelining now works

--- shell.c
+++ shell.c
@@ -76,6 +76,73 @@ void help() //Lists all custom commands
 	printf("> : redirect output of process to a file\n");
 	printf("clear : clear the visible area of shell\n");
 	printf("Ctrl+C (^C) : stop the current process ; doesn't do anything if no ongoing process\n");
+	printf(" | : single level pipe-lining (without any file I/O redirection,cd in between\n");
+}
+
+void loop_pipe(char *a,char* b) 
+{
+  int   p[2];
+  pid_t pid;
+  int   fd_in = 0;
+  char*** cmd;
+  cmd=(char***)malloc(sizeof(cmd)*3);
+  char *tok1,*tok2;
+  int i,j;
+  char* tempo[100];
+  char* tempo2[100];
+  i=0;j=0;
+  tok1=strtok(a," ");
+  while(tok1!=NULL) 
+  {
+	tempo[i++]=strdup(tok1);
+	tok1=strtok(NULL," ");
+   }
+  tempo[i]=NULL;
+  j=0;
+  tok2=strtok(b," ");
+  while(tok2!=NULL)
+  {
+  	tempo2[j++]=strdup(tok2);
+  	tok2=strtok(NULL," ");
+  }
+  tempo2[j]=NULL;
+  cmd[0]=tempo;
+  cmd[1]=tempo2;
+  cmd[2]=NULL;
+  while (*cmd != NULL)
+    {
+      pipe(p);
+      if ((pid = fork()) == -1)
+        {
+          exit(EXIT_FAILURE);
+        }
+      else if (pid == 0)
+        {
+          dup2(fd_in, 0); //change the input according to the old one 
+          if (*(cmd + 1) != NULL)
+            dup2(p[1], 1);
+          close(p[0]);
+          if(!strcmp((*cmd)[0],"history")) //'history' command 
+		  {
+			display();
+			exit(EXIT_FAILURE);
+		  }
+		  else if(!strcmp((*cmd)[0],"help")) //'help' command
+		  {
+			help();
+			exit(EXIT_FAILURE);
+		  }
+          execvp((*cmd)[0], *cmd);
+          exit(EXIT_FAILURE);
+        }
+      else
+        {
+          wait(NULL);
+          close(p[1]);
+          fd_in = p[0]; //save the input for the next command
+          cmd++;
+        }
+    }
 }
 
 int main()
@@ -88,10 +155,27 @@ int main()
 		fgets(LEL,100000,stdin); //Read input
 		i=0;
 		LEL[strlen(LEL)-1]='\0'; //Replace newline with \0
+		if(strstr(LEL,"|")!=NULL)
+		{
+			char *x,*y;
+			insert(LEL); //Insert to history, add only non=empty commands
+			tok=strtok(LEL,"|");
+			x=strdup(tok);
+			tok=strtok(NULL,"|");
+			if(tok==NULL)
+			{
+				printf("Second argument of pipe missing.\n");
+				continue;
+			}
+			y=strdup(tok);
+			// printf("Passing2 %s\n",y);
+			loop_pipe(x,y);
+			continue;
+		}
 		tok=strtok(LEL," "); //Break by space
 		if(tok==NULL) continue; //If blank,ignore (to avoid segmentation fault)
-		insert(LEL); //Insert to history, add only non=empty commands
 		if(!strcmp("exit",strdup(tok))) break; //exit 
+		insert(LEL); //Insert to history, add only non=empty commands
 		while(tok!=NULL) 
 		{
 			temp[i++]=strdup(tok);
